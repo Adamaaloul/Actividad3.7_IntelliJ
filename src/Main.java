d4875eadf7f8de254f628eb413fe8d5c1e88edc7
Adam Aaloul Izariahen <adamakloul@gmail.com>
2023-12-19 19:04:07 +0100

    Primer commit -Proyecto .JAVA

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola, estamos en IntelliJ");
+
+        imprimirNumeros();
+    }
+    public static void imprimirNumeros() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
